b5a0d857bcb0494c1353b54ddd68cce69d636262
arvindayal <arvinddayal@gmail.com>
2014-01-23 14:05:05 -0700

    submit form not functioning, button default removed

--- mainjava.js
+++ mainjava.js
@@ -4,8 +4,38 @@ var load = function() {
 	var toggleInput = function() {
 		$('.input-form').toggle('display');
 	};
-$('#editor').on('click', toggleInput); 
 
+	$('#editor').on('click', toggleInput); 
+
+	var updateName = function() {
+		var x = $('#name').val();
+		$('name-info').text(x);
+	};
+	
+	var updateBio = function() {
+		var x = $('#bio').val();
+		$('bio-info').text(x);
+	};	
+
+	var updateBooks = function() {
+		var x = $('#books').val();
+		$('books-info').text(x);
+	};
+
+	var updateLibraries = function() {
+		var x = $('libraries').val();
+		$('libraries-info').text(x);
+	};
+
+
+
+
+
+
+	$('#submit-changes').click(function(event) {
+		event.preventDefault();
+
+	});
 
 
 
@@ -13,3 +43,15 @@ $('#editor').on('click', toggleInput);
 };
 
 $(document).on('ready', load);
+
+// form
+// #name
+// #bio
+// #books
+// #libraries
+
+// page
+// #name-info
+// #bio-inf
+// #books-info
+// #libraries-info
